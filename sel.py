10e6db92d4ce702a2fd0dd60dfb51f4cb9df088f
merrycatch22 <tarn.nicholas@gmail.com>
2019-06-18 16:27:04 -0700

    add extension

--- sel.py
+++ sel.py
@@ -1,4 +1,8 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
-browser = webdriver.Firefox()
+chrome_options = Options()
+chrome_options.add_argument("--disable-infobars")
+chrome_options.add_extension(r'C:\Users\tarn\workspace\uBlock-Origin_v1.20.0.crx')
+browser = webdriver.Chrome(chrome_options=chrome_options)
 browser.get('http://bluffave.com/')
