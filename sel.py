3123b98815501bae53809aa382e45921899c953d
merrycatch22 <tarn.nicholas@gmail.com>
2019-06-19 13:25:01 -0700

    updates to selenium bluffave, can login only for now

--- sel.py
+++ sel.py
@@ -1,8 +1,36 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+# from time import sleep
+from selenium.webdriver.support.ui import WebDriverWait
 
-chrome_options = Options()
-chrome_options.add_argument("--disable-infobars")
-chrome_options.add_extension(r'C:\Users\tarn\workspace\uBlock-Origin_v1.20.0.crx')
-browser = webdriver.Chrome(chrome_options=chrome_options)
-browser.get('http://bluffave.com/')
+def loginToBluffave(browser):
+    browser.get('http://www.bluffave.com/')
+    assert 'Play your own private online poker cash games and tournaments! - Bluff Avenue' in browser.title
+    bluffaveUser = browser.find_element_by_xpath('//*[@id="loginForm_email"]')
+    bluffaveUser.send_keys(creds[0])
+    
+    bluffavePass = browser.find_element_by_xpath('//*[@id="loginForm_password"]')
+    bluffavePass.send_keys(creds[1]+'\n')
+
+
+if __name__ == '__main__':
+
+    file = open(".creds","r")
+    creds=file.read().splitlines()
+
+    chrome_options = Options()
+    chrome_options.add_argument("--disable-infobars")
+    chrome_options.add_extension('uBlock-Origin_v1.20.0.crx')
+    browser = webdriver.Chrome('chromedriver.exe',chrome_options=chrome_options)
+    try:
+        browser.set_window_position(0,0)
+        browser.set_window_size(1360,730)
+        browser.maximize_window()
+
+        loginToBluffave(browser)
+    except AssertionError:
+        print("AssertionError, title is wrong")
+        browser.quit()
+    except:
+        print("other browser failure")
+        browser.quit()
