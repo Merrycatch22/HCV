140b8b763a5de10e916ddf72626ed4fafc29df43
merrycatch22 <tarn.nicholas@gmail.com>
2019-06-19 20:57:45 -0700

    sel can be used well now, needs docu and integration with ahk?

--- sel.py
+++ sel.py
@@ -1,8 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-# from time import sleep
+from time import sleep
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.action_chains import ActionChains
+
+
 
 def isBluffaveHomepage(browser):
     return browser.title=='Home Page - Bluff Avenue'
@@ -18,28 +20,30 @@ def loginToBluffave(browser):
     
     WebDriverWait(browser, 10).until(isBluffaveHomepage)
     
-def openGames(browser):
+def openGames(browser,urls):
     i=2
     bluffaveGames = browser.find_elements_by_xpath('//*[@id="homePlayingFriendsBox"]/div/div[2]/div/div[2]/table/tbody/tr['+str(i)+']/td[4]/a')
     
     while len(bluffaveGames)>0:
         bluffaveGames[0].click()
-        print(browser.current_url)
-        
-        tablesPlayingThisGame = WebDriverWait(browser,10).until(lambda x : x.find_element_by_xpath('//*[@id="cashLobby"]/embed'))
-        clickFlash = ActionChains(browser)
-        clickFlash.move_to_element_with_offset(tablesPlayingThisGame,190,228)
-        clickFlash.pause(3)
-        clickFlash.click()
-        clickFlash.pause(3)
-        clickFlash.click()
-        clickFlash.perform()
+        # print(browser.current_url)
+        if browser.current_url not in urls:
+            urls.append(browser.current_url)
+            tablesPlayingThisGame = WebDriverWait(browser,10).until(lambda x : x.find_element_by_xpath('//*[@id="cashLobby"]/embed'))
+            clickFlash = ActionChains(browser)
+            clickFlash.move_to_element_with_offset(tablesPlayingThisGame,190,228)
+            clickFlash.pause(3)
+            clickFlash.click()
+            clickFlash.pause(3)
+            clickFlash.click()
+            clickFlash.perform()
         
         browser.get('http://www.bluffave.com/')
         WebDriverWait(browser, 10).until(isBluffaveHomepage)
         i+=1
         bluffaveGames = browser.find_elements_by_xpath('//*[@id="homePlayingFriendsBox"]/div/div[2]/div/div[2]/table/tbody/tr['+str(i)+']/td[4]/a')
 
+    return urls
 if __name__ == '__main__':
 
     file = open(".creds","r")
@@ -52,15 +56,20 @@ if __name__ == '__main__':
     browser = webdriver.Chrome('chromedriver.exe',chrome_options=chrome_options)
         
     try:
-        browser.set_window_position(0,0)
-        browser.set_window_size(1360,730)
-        browser.maximize_window()
-
+        # browser.minimize_window()
+        # browser.set_window_position(0,0)
+        # browser.set_window_size(1360,730)
+        
+        urls=[]
         loginToBluffave(browser)
-        openGames(browser)
+        # urls = openGames(browser,urls)
+        # print(urls)
+        while True:
+            urls = openGames(browser,urls)
+            sleep(60)
     except AssertionError:
         print("AssertionError, title is wrong")
         browser.quit()
-    except:
-        print("other browser failure")
-        browser.quit()
+    # except:
+    #     print("other browser failure")
+    #     browser.quit()
